a3ea7d16080b52b952184a6bed59283b15312886
Garoma Aga Bullo <garoma.bullo@student.wsb.edu.pl>
2026-01-14 08:12:47 +0100

    Update app title

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Board from "./components/Board";
 export default function App() {
   return (
     <div className="app">
-      <h1>Single Page Kanban-Task Management System</h1>
+      <h1>Kanban-Task Management System</h1>
       <Board />
     </div>
   );
